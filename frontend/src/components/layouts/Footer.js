f4edf38c5ab484b7757948296e5cffb260a6ba21
Ajish <ajish.r@infospica.com>
2023-11-23 11:33:36 +0530

    update

---
+++ frontend/src/components/layouts/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer class="py-1">
+      <p class="text-center text-dark mt-1">
+        JVLcart - 2022-2023, All Rights Reserved
+      </p>
+    </footer>
+  )
+}
